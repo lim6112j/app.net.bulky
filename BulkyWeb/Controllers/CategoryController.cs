7acb5f81b53fb202bbb15fba0cf9e4d73720d8f9
lim6112j <lim6112j@gmail.com>
2023-05-12 01:00:46 +0900

    db accessed, page designed

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,10 +1,26 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
     public IActionResult Index()
+    {
+        List<Category> categories = _db.Categories.ToList();
+        Console.WriteLine(categories);
+        return View(categories);
+    }
+
+    public IActionResult Create()
     {
         return View();
     }
