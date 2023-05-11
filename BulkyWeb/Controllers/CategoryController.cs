c601c209c7a720b1eafa3180d9a005016622250a
ben lim <lim6112j@gmail.com>
2023-05-11 19:07:45 +0900

    db connected

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
